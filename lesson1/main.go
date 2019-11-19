dd6aad70276b4b1bd6330ec8f9203e3453df1a1a
akaKAIN <mkain@mail.ru>
2019-11-20 08:44:57 +1000

    Добавлени генерации Int-диапазона

--- lesson1/main.go
+++ lesson1/main.go
@@ -1,25 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"time"
-)
+import "task_4"
 
 func main(){
 	//fmt.Printf("Сумма=%d\n\n", task_1.SumTreeDigit())
 	//task_2.BoolOperations()
 	//task_3.TwoPoints()
 	//task_3.TwoPoints()
-	type inp interface {}
-	//var n inp
-	var name int
-StartLoop:
-	time.Sleep(2*time.Second)
-	if _, err := fmt.Scan(&name); err != nil {
-		log.Printf("Ошибка ввода данных: %s", err)
-		goto StartLoop
-	}
-	fmt.Printf("Type=%T, val=%v", name, name)
+	task_4.MyRange()
 
 }
--- lesson1/src/task_4/task_4.go
+++ lesson1/src/task_4/task_4.go
@@ -1,10 +1,16 @@
 package task_4
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"time"
+)
 
-type Inserting interface {}
 
 func MyRange(){
+	rand.Seed(time.Now().UnixNano())
 	fmt.Print("Написать программу, которая генерирует в указанных пользователем границах\n" +
 	"●	случайное целое число,\n" +
 	"●	случайное вещественное число,\n" +
@@ -13,16 +19,56 @@ func MyRange(){
 		"Например, если надо получить случайный символ от 'a' до 'f',\n" +
 		"то вводятся эти символы. Программа должна вывести на экран любой\n" +
 	"символ алфавита от 'a' до 'f' включительно.\n\n")
-
-	fmt.Print("Введите начало диапазона (символ/цифру)")
-
+Menu:
+	fmt.Print("Выберите тип, желаемого диапазона:\n1. Случайное целое число.\n" +
+		"2. Случайное вещественное число.\n3. случайный символ.\n\n>>>")
+	var choise uint8
+	_, err := fmt.Scan(&choise)
+	if err != nil {
+		log.Printf("Error choise: %s", err)
+	}
+	switch choise {
+	case 1:
+		NumberFunc()
+	case 2:
+		FloatFunc()
+	case 3:
+		//SimbolFunc()
+	default:
+		fmt.Print("Неверный выбор пункта меню.")
+		goto Menu
+	}
 }
 
-func Insert(msg string)  {
-	var input Inserting
-	fmt.Printf("%s\n", msg)
-
-
-	return
+func NumberFunc(){
+IntLoop:
+	fmt.Print("Введите через пробел верхний и нижние диапазоны\n")
+	var min, max int
+	_, err := fmt.Scan(&min, &max)
+	if err != nil {
+		log.Printf("Ошибка ввода int диапазона: %s", err)
+		goto IntLoop
+	}
+	if min > max {
+		min, max = max, min
+	}
+	numInRange := min + rand.Intn(max - min + 1)
+	fmt.Printf("done. => [%d;%d]=%d\n", min, max, numInRange)
 }
 
+func FloatFunc(){
+FloatLoop:
+	fmt.Print("Введите через пробел верхний и нижние диапазоны\n")
+	var min, max float32
+	_, err := fmt.Scan(&min, &max)
+	if err != nil {
+		log.Printf("Ошибка ввода int диапазона: %s", err)
+		goto FloatLoop
+	}
+	if min > max {
+		min, max = max, min
+	}
+	floatInRange := rand.Float64()
+	fmt.Printf("========\n=%v\n=%v\n=======", floatInRange, math.Pow10(6))
+
+}
