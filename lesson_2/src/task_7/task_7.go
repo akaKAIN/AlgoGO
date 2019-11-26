5e7c7c31a607a02a5f96567418be4d984fcf1d44
akaKAIN <mkain@mail.ru>
2019-11-27 08:17:29 +1000

    Выполнил задание №7

--- lesson_2/src/task_7/task_7.go
+++ lesson_2/src/task_7/task_7.go
@@ -1,1 +1,31 @@
 package task_7
+
+import (
+	"fmt"
+	"log"
+)
+
+/*"""
+7.	Напишите программу, доказывающую или проверяющую, что для множества
+натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
+ где n - любое натуральное число.
+"""
+*/
+
+func Pifag() {
+	var input, sum, pif int
+	if _, err := fmt.Scan(&input); err != nil {
+		log.Printf("Ошибка %s, %v", err, input)
+	}
+	for i := 0; i <= input; i++ {
+		sum += i
+	}
+	pif = input*(input+1)/2
+	fmt.Println("Сумма чисел 1 + 2 + ... + n =", sum)
+	fmt.Println("Выражение вида n * (n + 1) / 2 =", pif)
+	if sum == pif {
+		fmt.Println("Равенство доказано.")
+	}else{
+		fmt.Println("Странно, но они оказались неравны ...")
+	}
+}
