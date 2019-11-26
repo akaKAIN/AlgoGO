fcb02ceb8b03b0f26364e01b45142991e2dac335
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2019-11-26 18:06:23 +1000

    Выполнил задание №3

--- lesson_2/src/task_3/task_3.go
+++ lesson_2/src/task_3/task_3.go
@@ -1,1 +1,26 @@
 package task_3
+
+import (
+	"fmt"
+	"log"
+)
+
+/*"""
+3.	Сформировать из введенного числа обратное по порядку входящих в него
+цифр и вывести на экран. Например, если введено число 3486,
+ то надо вывести число 6843.
+"""
+ */
+
+func ReversPrint(){
+	var input int
+	fmt.Print("Введите число, для вывода его отзеркаленной версии.")
+	if _, err := fmt.Scan(&input); err != nil {
+		log.Printf("Ошибка ввода данных: %s;\ninput=%v", err, input)
+	}
+	for input > 0 {
+		fmt.Print(input%10)
+		input /= 10
+	}
+
+}
