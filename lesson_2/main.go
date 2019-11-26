9a9246eca8b3a2fd680a3f52b507871acd1a71ea
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2019-11-26 18:58:34 +1000

    Выполнил задание №5

--- lesson_2/main.go
+++ lesson_2/main.go
@@ -1,9 +1,10 @@
 package main
 
-import "task_1"
-
 func main () {
-	task_1.Calc()
-
+	//task_1.Calc()
+	//task_2.CountSameNumbers()
+	//task_3.ReversPrint()
+	//task_4.HalfDivRow()
+	//task_5.AckiiTable()
 
 }
--- lesson_2/src/task_5/task_5.go
+++ lesson_2/src/task_5/task_5.go
@@ -1,1 +1,28 @@
 package task_5
+
+import "fmt"
+
+/*"""
+5.	Вывести на экран коды и символы таблицы ASCII, начиная с символа
+под номером 32 и заканчивая 127-м включительно.
+Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
+"""
+ */
+
+func AckiiTable(){
+	var count = 1
+	var sep, rowSep string
+
+	for i:=33; i<127; i++{
+
+		rowSep=""
+		if count % 10 == 0 {
+			rowSep = "\n"
+			sep = ""
+		}else if count % 10 == 1{
+			sep = "\t"
+		}
+		fmt.Print(string(i),"-", count, sep, rowSep)
+		count++
+	}
+}
