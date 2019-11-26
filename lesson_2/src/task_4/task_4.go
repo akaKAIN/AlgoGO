040462f683ed0f10a81b36c6f4c2a8693a361c9a
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2019-11-26 18:15:07 +1000

    Выполнил задание №4

--- lesson_2/src/task_4/task_4.go
+++ lesson_2/src/task_4/task_4.go
@@ -1,1 +1,26 @@
 package task_4
+
+import (
+	"fmt"
+	"log"
+)
+
+/*"""
+4.	Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
+Количество элементов (n) вводится с клавиатуры.
+"""
+ */
+
+func HalfDivRow(){
+	var input int
+	var result float64
+	result = 1
+
+	if _, err := fmt.Scan(&input); err != nil {
+		log.Printf("Ошибка ввода: %s, input=%v", err, input)
+	}
+	for i:=1; i<input; i++{
+		result /= 2
+	}
+	fmt.Println(result)
+}
