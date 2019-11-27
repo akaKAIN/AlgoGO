dbf9914709b4ae2bd9c1be6694a5f73fadd5f2ce
akaKAIN <mkain@mail.ru>
2019-11-27 21:16:58 +1000

    Сохранение перед конфликтами

--- lesson_2/main.go
+++ lesson_2/main.go
@@ -1,10 +1,16 @@
 package main
 
+import (
+	"task_8"
+)
+
 func main () {
 	//task_1.Calc()
 	//task_2.CountSameNumbers()
 	//task_3.ReversPrint()
 	//task_4.HalfDivRow()
 	//task_5.AckiiTable()
-
+	//task_6.Vanga()
+	//task_7.Pifag()
+	task_8.Counter()
 }
