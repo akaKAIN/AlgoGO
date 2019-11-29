673f934d213b8ff359e006c10d140b122ee5cab8
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2019-11-29 15:26:28 +1000

    Выполнил Задание №5

--- lesson_3/src/task_5/task_5.go
+++ lesson_3/src/task_5/task_5.go
@@ -1,1 +1,33 @@
 package task_5
+
+import (
+	"fmt"
+)
+
+/*
+В массиве найти максимальный отрицательный элемент.
+# Вывести на экран его значение и позицию (индекс) в массиве.
+ */
+
+type Point struct {
+	Ind int
+	Val int
+}
+
+func MaxNegativeNumber() {
+	var num Point
+	randArr := []int{1, 2, -3, -4, -5, 10, -8, -44}
+	for key, val := range randArr {
+		if val < 0 && val < num.Val {
+			num = Point{key, val}
+		}
+	}
+	if num.Val >= 0 {
+		fmt.Println("Отлицательныз чисел не найдено в массиве.")
+	}else{
+		fmt.Printf("Самое большое отрицательное = %d по индексу %d\n", num.Val, num.Ind)
+	}
+	fmt.Println(randArr)
+
+
+}
