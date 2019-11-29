5fc643e35cc5f3e82c3a08a3d764d48147b60c79
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2019-11-29 17:02:53 +1000

    Check Point

--- lesson_3/main.go
+++ lesson_3/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "task_4"
+import "task_7"
 
 func main()  {
 	//task_1.MultiplicationCount()
 	//task_2.SortArray()
 	//task_3.ChangeMinMax()
-	task_4.FrequentCounter()
+	//task_4.FrequentCounter()
+	//task_5.MaxNegativeNumber()
+	//task_6.RangeSum()
+	task_7.MinTwoNum()
+
 }
