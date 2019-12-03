844eb0140e5384d409c72193ef65e8d6eb6ae2a5
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2019-12-03 18:58:50 +1000

    CheckPoint

--- lesson_4/src/task_1/task_1.go
+++ lesson_4/src/task_1/task_1.go
@@ -1,7 +1,19 @@
 package task_1
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func init () {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func Sieve () {
-
+	var arr []int
+	for i:= 1; i<1e6; i++{
+		arr = append(arr, i)
+	}
+	fmt.Println(len(arr))
 }
