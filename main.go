ad24738e98acf9279c2a3dcc819b5e6c15be1621
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2019-12-06 18:09:48 +1000

    Begin os_GUI for web-server

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+/*
+Выборка первых элементов файлов лежащих в указаном каталоге.
+Каналы
+Паралеллизм
+Чтение файлов
+Интерфейсы?
+ */
+
+const sepr  = os.PathSeparator
+
+
+func main() {
+	var userDir, path string
+	var err error
+	if _, err := fmt.Scanln(&userDir); err != nil && userDir !="" {
+		log.Printf("Error input: %s. Err:%s", userDir, err)
+		os.Exit(1)
+	}
+
+	if userDir == "" {
+		path, err = os.Getwd()
+	}else{
+		 if err = os.Chdir(userDir); err != nil {
+		 	log.Printf("Указан неверный путь: %s. Error: %s\n", userDir, err)
+		 }
+	}
+
+
+
+	fmt.Println(userDir)
+	dir, _ := os.Getwd()
+	fmt.Println(dir, path)
+
+}
